c68c3abd1f985993a0c936c23a7c9596e54a7132
Julio <julio.vinicius0816@gmail.com>
2026-02-19 23:00:26 -0300

    feat: Refactor timeout calculation to use uniform distribution and percentiles, capture training history, and add VS Code Python interpreter settings.

--- asynchronous/main.py
+++ asynchronous/main.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 import numpy as np
 from client import Client
-from scipy.stats import triang
+from scipy.stats import uniform
 from server import Server
 
 def load_data():
@@ -25,18 +25,26 @@ def split_data_random(train_data, num_clients):
     split_datasets = []
     start = 0
     for size in split_sizes:
-        split_datasets.append(shuffled_data.skip(start).take(size))
+        split_datasets.append(shuffled_data.skip(start) .take(size))
         start += size
 
     return split_datasets
 
-def main(num_clients, num_updates, timeout, epochs, batch_size):
+def get_percentile_timeout(percentile):
+    connection_dist = uniform(loc=0.1, scale=5)
+    train_dist = uniform(loc=0.1, scale=5)
+    connection_samples = connection_dist.rvs(1_000_000)
+    train_samples = train_dist.rvs(1_000_000)
+    sum_samples = connection_samples+train_samples
+    return np.percentile(sum_samples, percentile)
 
+def main(num_clients, num_updates, epochs, batch_size):
+    accuracy_history = []
     number_of_clients = num_clients
     number_of_updates = num_updates
-    timeout = timeout
     local_epochs = epochs
     batch_size = batch_size
+    percentile_timeout = get_percentile_timeout(25)
 
     training_data, testing_data = load_data()
     training_data_clients = split_data_random(training_data, number_of_clients)
@@ -47,22 +55,20 @@ def main(num_clients, num_updates, timeout, epochs, batch_size):
 
     server.create_model()
     server.setup_clients()
-    server.start_training()
+    local_history = server.start_training()
+    accuracy_history.extend(local_history)
+
 
 
 if __name__ == "__main__":
-    min_time_value = 0.1
-    max_time_value = 5
-    min_value = min_time_value*2
-    max_value = max_time_value*2
-    moda = (min_time_value + max_time_value)/2+(min_time_value+max_time_value)/2
-    c = (moda - min_value)/(max_value - min_value)
-    timeout_sync = triang.ppf(0.50, c, loc=min_value, scale=max_value-min_value)
+
+    
+    # print(percentile_samples)
     num_clients = 5
     num_updates = 5
-    print(f'timeout definido para 50%: {timeout_sync}')
+    print(f'timeout definido para 50%: {percentile_timeout}')
     # timeout = 20
     epochs = 1
     batch_size = 32
 
-    main(num_clients, num_updates, timeout_sync, epochs, batch_size)
+    main(num_clients, num_updates, percentile_timeout, epochs, batch_size)
