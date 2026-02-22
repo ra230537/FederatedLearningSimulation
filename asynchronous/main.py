3927eaff3e73647cdc92fd4f2bf396352deb7213
Julio <julio.vinicius0816@gmail.com>
2026-02-21 22:10:43 -0300

    feat: Implement CNN model and adjust data preprocessing for synchronous and asynchronous simulations, and add a report discrepancy analysis document.

--- asynchronous/main.py
+++ asynchronous/main.py
@@ -10,8 +10,8 @@ from constants import *
 
 def load_data():
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    x_train = x_train.reshape(-1, 784).astype("float32") / 255.0
-    x_test = x_test.reshape(-1, 784).astype("float32") / 255.0
+    x_train = x_train.reshape(-1, 28, 28, 1).astype("float32") / 255.0
+    x_test = x_test.reshape(-1, 28, 28, 1).astype("float32") / 255.0
     train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
     test_data = tf.data.Dataset.from_tensor_slices((x_test, y_test))
     return train_data, test_data
--- asynchronous/server.py
+++ asynchronous/server.py
@@ -28,7 +28,19 @@ class Server:
 
     def create_model(self):
         self.global_model = tf.keras.models.Sequential([
-            tf.keras.layers.Dense(128, activation='relu', input_shape=(784,)),
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Dropout(0.25),
+
+            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Dropout(0.25),
+
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dropout(0.5),
             tf.keras.layers.Dense(10, activation='softmax')
         ])
         self.global_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
